5e05f35dd0239955a1be546ca0c50d43cece0472
MillaDom <milladom@gmail.com>
2023-02-09 00:00:44 +0200

    tests added

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -2,7 +2,9 @@ package ru.netology;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
+import javax.swing.*;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -15,5 +17,42 @@ public class CardOrderTest {
         $("[data-test-id=agreement]").click();
         $("button[type=button]").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldHaveNameFieldsFilled(){
+        open("http://localhost:7777");
+        $("[data-test-id=phone] input").setValue("+79876543211");
+        $("[data-test-id=agreement]").click();
+        $("button[type=button]").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldHavePhoneFieldsFilled(){
+        open("http://localhost:7777");
+        $("[data-test-id=name] input").setValue("Казимир Алмазов");
+        $("[data-test-id=agreement]").click();
+        $("button[type=button]").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldHaveCyrillicName(){
+        open("http://localhost:7777");
+        $("[data-test-id=name] input").setValue("Kazimir Almazov");
+        $("[data-test-id=phone] input").setValue("+79876543211");
+        $("[data-test-id=agreement]").click();
+        $("button[type=button]").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldHaveCheckBoxPressed(){
+        open("http://localhost:7777");
+        $("[data-test-id=name] input").setValue("Казимир Алмазов");
+        $("[data-test-id=phone] input").setValue("+79876543211");
+        $("button[type=button]").click();
+        $("[data-test-id=agreement]").shouldHave(cssClass("input_invalid"));
     }
 }
