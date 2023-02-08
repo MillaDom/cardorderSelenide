582be4260f0c7ef99a893e987502a13c6626e759
MillaDom <milladom@gmail.com>
2023-02-08 18:34:04 +0200

    positive test added

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,19 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+    @Test
+    void shouldSubmitForm(){
+        open("http://localhost:7777");
+        $("[data-test-id=name] input").setValue("Казимир Алмазов");
+        $("[data-test-id=phone] input").setValue("+79876543211");
+        $("[data-test-id=agreement]").click();
+        $("button[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
